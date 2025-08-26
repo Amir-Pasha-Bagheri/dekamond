0c23123231c2be1949ba5e6a878fec79c273a1d6
Amir Pasha <a.p.bagheri83@gmail.com>
2025-08-26 19:37:33 +0330

    update configs

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import dynamic from 'next/dynamic';
+
+const DynamicDashboard = dynamic(() => import('./dashboard/page'), { ssr: false });
+
+function Page() {
+  return <DynamicDashboard />;
+}
+
+export default Page;
